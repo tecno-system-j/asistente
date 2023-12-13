c8ba6e03a2d60e6e60c52df90481722ab2454618
tecno-system-j <morcillo.juliand10@gmail.com>
2023-12-13 15:41:25 -0500

    se modifico app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 # Contraseña para acceder a /skynet
 skynet_password = '201001201'
@@ -34,6 +36,28 @@ def skynet():
 def skynet_main():
     return render_template('skynet_interface.html')
 
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('user_input')
+def handle_user_input(data):
+    # Importa tus funciones de asistente aquí
+    # from assistant_functions import iniciar_asistente
+
+    # Aquí procesas la entrada del usuario y obtienes la respuesta de la IA
+    user_input = data['input']
+    # Llama a tu función de asistente virtual y obtén la respuesta
+    # response = iniciar_asistente(user_input)
+    response = "Respuesta de la IA"
+
+    # Envía la respuesta de la IA al cliente
+    socketio.emit('response', {'response': response})
+
 if __name__ == '__main__':
     # Puerto para la interfaz principal
-    app.run(port=5000, debug=True)
+    socketio.run(app, port=5000, debug=True)
